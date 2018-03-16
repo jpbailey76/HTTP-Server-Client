d0d9b767d1d8f37f94223d5340fc3fcd1bdda5c0
Jeremy <jpb42@students.uwf.edu>
2018-03-15 21:38:25 -0500

    Revert "Documentation"
    This reverts commit 04afd71ac6c6a108c63549bd8f09b08d8bb3f908.

--- httpServer.c
+++ httpServer.c
@@ -20,13 +20,13 @@
 #define YELLOW   "\x1B[33m"
 #define RESET "\x1B[0m"
 
-const char HEADER[] = "HTTP/1.1 200 OK		\n\
+const char HEAD[] = "HTTP/1.1 200 OK		\n\
 										Server: Hostname		\n\
 										Content-Length: %ld		\
 									\nConnection: close			\
 									\nContent-Type: %s\n\n";
 
-const char ERROR_HEADER[] = "HTTP/1.1 403 Forbidden\n\
+const char ERROR_HEAD[] = "HTTP/1.1 403 Forbidden\n\
 													Server: Hostname\n 			 \
 													Content-Length:7 				 \
 												\nConnection: close				 \
@@ -42,14 +42,12 @@ int main(int argc, char **argv)
 	struct sockaddr_in serverAddr;
 	char request[BUFF_SIZE];
 
-	// Check that the program is being ran in a valid manner
 	if(argc != 1) 
 	{
 		puts("Usage: ./httpserver\n");
 		return ERROR;
 	}
 
-	// Create and bind the server socket
 	memset(&serverAddr, 0, sizeof(serverAddr));
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -62,18 +60,14 @@ int main(int argc, char **argv)
 	printf("Server address: [%d]\n", serverAddr.sin_addr.s_addr);
 	printf("Server port: [%d]\n", PORT);
 
-	// Receive connections
 	while(1)
 	{
-		// Accept the client 
 		clientSockfd = accept(serverSockfd, NULL, NULL);
 		memset(request, 0, sizeof(request));
 
-		// Read their request
 		read(clientSockfd, request, BUFF_SIZE);
 		printf("\n%s\n", request);
 
-		// Create a new process for responding
 		pid = fork();
 		if(pid == 0)
 		{
@@ -81,59 +75,52 @@ int main(int argc, char **argv)
 			char *path;
 			path = calloc(BUFF_SIZE, sizeof(char));
 
-			// Verify that we're receiving a valid request
 			if(checkHeader(request, path) == SUCCESS)
 			{
 				FILE *fp;
-
-				// Verify that the file requested exists
 				int fileSize = getFileSize(path);
 
 				printf("\nFilesize: [%d]\n", fileSize);
 				if(fileSize != ERROR)
 				{
-					// Verify that the extension of the file is valid
-					// We only handle html and jpg
 					if(verifyExtension(path) == ERROR)
 					{
 						printf("ERROR: File is not of .html or .jpg type.\n");
 						return ERROR;
 					}
 
-					// Open the requested file
 					if ((fp = fopen(path, "r")) == NULL)
 					{
 						printf("ERROR: Failed to open file - [%s]\n", path);
 						return ERROR;
 					}
 
-					// Get the file descriptor
-					int filefd = fileno(fp), size;
+					int fd = fileno(fp), size;
 					char buff[BUFF_SIZE];
-					if(filefd < 0)
-					{
-						printf("\nERROR: main() - Invalid file descriptor.\n");
-						return ERROR;
-					}
 
-					// Write our response to the client
 					printf("   RESPONSE:   \n"
 								 "===============\n");
 					if(strstr(path, ".html") != NULL)
-						sprintf(response, HEADER, (long)fileSize, "text/html");
+						sprintf(response, HEAD, (long)fileSize, "text/html");
 					else if(strstr(path, ".jpg") != NULL)
-						sprintf(response, HEADER, (long)fileSize, "image/jpeg");
+						sprintf(response, HEAD, (long)fileSize, "image/jpeg");
 					write(clientSockfd, response, strlen(response));
 
-					while((size = read(filefd, buff, BUFF_SIZE)) != 0) 
+					if(fd < 0)
+					{
+						printf("\nERROR: main() - File size.\n");
+						return ERROR;
+					}
+						
+					while((size = read(fd, buff, BUFF_SIZE)) != 0) 
 					{
 						write(clientSockfd, buff, size);
 						memset(buff, 0, strlen(buff));
 					}
 					fclose(fp);
 				}
-				else	// They sent us an invalid request, so we send them the error header.
-					write(clientSockfd, ERROR_HEADER, strlen(ERROR_HEADER));
+				else
+					write(clientSockfd, ERROR_HEAD, strlen(ERROR_HEAD));
 			}
 			free(path);
 			close(clientSockfd);
@@ -177,14 +164,12 @@ int getFileSize(char *path)
 	char buff[BUFF_SIZE];
 	FILE *fp;
 
-	// Open file at path
 	if ((fp = fopen(path, "r")) == NULL)
 	{
 		printf("ERROR: Failed to open file - [%s]\n", path);
 		return ERROR;
 	}
 
-	// Get the file descriptor
 	int fd = fileno(fp);
 	if (fd < 0)
 	{
@@ -192,7 +177,6 @@ int getFileSize(char *path)
 		return ERROR;
 	}
 
-	// Count the size
 	while((count = read(fd, buff, BUFF_SIZE)) != 0) 
 		result += count;
 
@@ -202,12 +186,10 @@ int getFileSize(char *path)
 
 int verifyExtension(char *path)
 {
-	// Parse the path until you reach the file extension
 	path++;
 	while(*path != '\0' && *path != '.') 
 		path++;
 
-	// Check if valid
 	if( (strcmp(path, ".html") == 0) 
 		||(strcmp(path, ".jpg")  == 0) )
 		return SUCCESS;
